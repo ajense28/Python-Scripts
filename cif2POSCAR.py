37fb032e76f02c6b3d598405beee63f7f7237d9b
ajense28 <154552356+ajense28@users.noreply.github.com>
2024-10-23 12:06:45 -0600

    Update cif2POSCAR.py
    added reference

--- cif2POSCAR.py
+++ cif2POSCAR.py
@@ -1,3 +1,4 @@
+# converts a .cif file to a POSCAR file with the option of creating a supercell with the input geometry (based on the script provided in the VASP MD tutorial).
 # to run: python3 cif2POSCAR.py
 from pymatgen.core import Structure
 
@@ -9,3 +10,5 @@ my_struc.make_supercell(n)
 
 # write supercell to POSCAR format with specified filename
 my_struc.to(fmt="poscar", filename=input("name of dump file (ie ./POSCAR or /home/output_POSCAR): ")) # dumps to same dir as file
+
+# reference: https://www.vasp.at/tutorials/latest/md/part1/
